ae68b529c61c2c2640664bccb5027c364cef866f
CleverCodder <m.saad4code@gmail.com>
2021-12-14 18:50:11 +0500

    Test The Surge

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -173,7 +173,7 @@ const NavBar = () => {
                                     smooth={true}
                                     offset={0}
                                     duration={500}
-                                >Contact</Link>
+                                >Contacttest</Link>
                                 <Link
                                     className='nav-text'
                                     activeClass="active"
